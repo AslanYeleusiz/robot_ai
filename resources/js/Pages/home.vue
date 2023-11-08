258faac620b2e8826a4fbd402c5682b895ccf815
Alex Alex <askon040@gmail.com>
2023-11-08 09:20:11 +0600

    fix 1

---
+++ resources/js/Pages/home.vue
@@ -0,0 +1,21 @@
+<template>
+    <layout>
+        asdsaas
+    </layout>
+</template>
+
+
+<script>
+    import layout from '@/Layouts/layout.vue'
+
+    export default {
+        components: {
+            layout,
+        },
+    }
+
+</script>
+
+<style lang="scss">
+
+</style>
